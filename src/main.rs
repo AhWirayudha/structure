26feeed68fc9f2c71a417cc94a71d45333cc1de0
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-04-29 15:03:47 +0700

    field init shorthand

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,12 @@ fn main() {
     // function
     let user3 = build_user(String::from("user3@example.com"), String::from("someusername123"));
     println!("{}", user3.email);
+
+    // function with field init shorthand
+    let mut user4 = build_user2(String::from("user4@example.com"), String::from("someusername123"));
+    println!("{}", user4.email);
+    user4.email = String::from("vLbZx@example.com");
+    println!("{}", user4.email);
 }
 
 // function
@@ -36,6 +42,16 @@ fn build_user(email: String, username: String) -> User {
     User {
         email: email,
         username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+// function with field init shorthand 
+fn build_user2(email: String, username: String) -> User {
+    User {
+        email,
+        username,
         active: true,
         sign_in_count: 1,
     }
